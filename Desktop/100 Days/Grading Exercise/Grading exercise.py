3da35a4e0f73e3fd475a4503c5e01fa15433891d
km_ochieng <stupidgeniuseske@gmail.com>
2024-02-27 07:16:08 +0300

    Grading results

---
+++ Desktop/100 Days/Grading Exercise/Grading exercise.py
@@ -0,0 +1,22 @@
+#The Grading Exercise
+student_scores= {
+    "Harry": 81,
+    "Ron": 78,
+    "Hermione": 99,
+    "Draco": 74,
+    "Neville": 62,
+}
+student_grades={}
+for student in student_scores:
+    if student_scores[student]>90:
+        student_grades[student]= "Outstanding"
+    elif student_scores[student]>80:
+        student_grades[student]= "Exceeds expectation"
+    elif student_scores[student]>70:
+        student_grades[student]= "Acceptable"
+    else:
+        student_grades[student]= "Fail"
+print (student_grades)
+for student in student_grades:
+    print (student)
+    print (student_grades[student])
